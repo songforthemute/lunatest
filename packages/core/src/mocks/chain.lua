1d46fe9f4e2bd34fc5d7cb96c17d59306fa66400
songforthemute <huni0423@naver.com>
2026-02-26 01:51:03 +0900

    feat(core-mocks): 상태 전이와 프로토콜 프리셋 3종 추가

--- packages/core/src/mocks/chain.lua
+++ packages/core/src/mocks/chain.lua
@@ -1,6 +1,14 @@
--- 체인 상태 템플릿
-return {
-  id = 1,
-  name = "Ethereum",
-  blockNumber = 0,
-}
+-- 체인 상태 관리
+local M = {}
+
+function M.new(input)
+  local value = input or {}
+  return {
+    id = value.id or "0x1",
+    name = value.name or "Ethereum",
+    blockNumber = value.blockNumber or 0,
+    gasPrice = value.gasPrice or "0x0",
+  }
+end
+
+return M
